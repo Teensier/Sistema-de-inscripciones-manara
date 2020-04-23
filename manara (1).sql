3c40703a8100213eb738f8a0c6c75c7e03faec20
JSEGURA <51034560+Teensier@users.noreply.github.com>
2020-04-22 21:56:26 -0500

    Add files via upload

---
+++ manara (1).sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2020 at 04:55 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `manara`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `ID` int(10) NOT NULL,
+  `Nombre_Alumno` varchar(30) NOT NULL,
+  `Apellido_Alumno` varchar(30) NOT NULL,
+  `Edad` int(3) NOT NULL,
+  `Fecha_Inscripcion` date NOT NULL,
+  `Nombre_Padre` varchar(60) NOT NULL,
+  `Nombre_Madre` varchar(60) NOT NULL,
+  `Costo` int(10) NOT NULL,
+  `Nivel_Curso` int(3) NOT NULL,
+  `Numero_Telefono` int(10) NOT NULL,
+  `Correo` varchar(50) NOT NULL,
+  `Status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `ID` int(11) NOT NULL,
+  `Nombre_Curso` varchar(30) NOT NULL,
+  `Nivel` int(11) NOT NULL,
+  `Costo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `ID` int(10) NOT NULL,
+  `Descripcion` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
